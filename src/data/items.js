77ef18f8471277a96e2f80beff1c5174967aba80
kels-orien <kelvinorien@gmail.com>
2019-07-07 23:25:18 +0100

    Hardcoded data for app testing

---
+++ src/data/items.js
@@ -0,0 +1,111 @@
+export const phones = [
+  {
+    name: "Redmi Note 7",
+    brand: "xaomi",
+    price: "62,725",
+    cpu: "snapdragon 600 full blood version",
+    camera: "48.0MP + 5.0MP Dual Real Cameras, 13MP Front Camera",
+    Screen: "6.3-Inch 2340x1080P FHD+ 19.5:9 Screen ",
+    battery: "4000mah",
+    memory: "32gb  ( Support max 256GB TF Card, VFat Format Only)",
+    OS:
+      "Android 9.0 (Pie), Qualcomm SDM675 Snapdragon 675 (11 nm), Octa-core (2x2.0 GHz Kryo 460 Gold & 6x1.7 GHz Kryo 460 Silver), Adreno 612. ",
+    ram: "3GB",
+    sim: "Dual SIM 4G",
+    imageurl: "",
+    sourceurl: "",
+    description: "",
+    other: "WIFI, Bluetooth 5.0, GPS, OTG"
+  },
+  {
+    name: "IPhone XS Max",
+    brand: " Apple",
+    price: "439,900",
+    cpu: "Intelligent A12 Bionic",
+    camera:
+      "12MP dual cameras with dual OIS and 7MP TrueDepth front camera – Portrait ",
+    Screen: " 6.5-inch Super Retina display (OLED) with HDR ",
+    battery: "4000mah",
+    memory: "64GB",
+    OS: "iOS 12 with Memoji, Screen Time, Siri Shortcuts, and Group FaceTime",
+    ram: "4GB",
+    sim: "Dual SIM 4G",
+    imageurl: "",
+    sourceurl: "",
+    description: "",
+    other: "Wireless charging – works with Qi chargers"
+  },
+  {
+    name: "Infinix S4",
+    brand: " Infinix",
+    price: " 64,000",
+    cpu: "Octa Core 2.0GHz",
+    camera: "13MP+8MP+2MP Triple Rear Camera 32MP AI",
+    Screen: " 6.2-Inch HD+",
+    battery: "4000mah",
+    memory: "32GB",
+    OS: "Android 9 Pie",
+    ram: "3GB",
+    sim: "Dual SIM 4G",
+    imageurl: "",
+    sourceurl: "",
+    description: "",
+    other: ""
+  },
+  {
+    name: "Infinix HOT 7 Pro",
+    brand: " Infinix",
+    price: " 43, 400",
+    cpu: " MediaTek Quad Core",
+    camera: "13MP+8MP+2MP Triple Rear Camera 32MP AI",
+    Screen: " 6.2-Inch HD+",
+    battery: "4000mah",
+    memory: "32GB",
+    OS: "Android 9 Pie",
+    ram: "3GB",
+    sim: "Dual SIM 4G",
+    imageurl: "",
+    sourceurl: "",
+    description: "",
+    other: ""
+  },
+  {
+    name: "Tecno Spark 3",
+    brand: " Tecno",
+    price: " 31,400",
+    cpu: " Quad Core",
+    camera: "3MP Back Camera, 8MP Front Camera",
+    Screen: " 6.2-Inch HD",
+    battery: " 3500mAh Li-Ion",
+    memory: " 16GB ROM",
+    OS: "android 8.1 Oreo",
+    ram: "1GB",
+    sim: "Dual SIM 4G",
+    imageurl: "",
+    sourceurl: "",
+    description: "",
+    other: ""
+  },
+  {
+    name: "Apple IPhone XR",
+    brand: "Apple",
+    price: "274, 500",
+    cpu: " Quad Core",
+    camera:
+      "12MP camera with OIS and 7MP TrueDepth front camera - Portrait mode, Portrait Lighting, Depth Control, and Smart HDR",
+    Screen: "6.1-inch Liquid Retina display (LCD)",
+    battery: "  4000mah",
+    memory: " 16GB ROM",
+    OS: "iOS",
+    ram: "3GB",
+    sim: "Dual SIM 4G",
+    imageurl: "",
+    sourceurl: "",
+    description: "",
+    other: ""
+  }
+];
+
+export const headphones = [{}];
+
+export const laptops = [{}];
